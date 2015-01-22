4a62337098bdf7c6b3b5f4fb96f9715cecb2c9be
jinhyun <free1330@gmail.com>
2015-01-22 16:24:34 +0900

    feature: 엘리베이터 구동 재구현

--- src/test/TestElevatorOperate.java
+++ src/test/TestElevatorOperate.java
@@ -1,7 +1,5 @@
 package test;
 
-import org.junit.Test;
-
 /**
  * Created by Jinhyun on 2015. 1. 17..
  */
@@ -12,15 +10,6 @@ class ElevatorOperate implements Runnable {
     Elevator elevator = new Elevator();
 
     int reqFloorNumber;
-    boolean isPushUpBtn = false;
-
-    public boolean isPushUpBtn() {
-        return isPushUpBtn;
-    }
-
-    public void setPushUpBtn(boolean isPushUpBtn) {
-        this.isPushUpBtn = isPushUpBtn;
-    }
 
     public int getReqFloorNumber() {
         return reqFloorNumber;
@@ -35,6 +24,7 @@ class ElevatorOperate implements Runnable {
         System.out.println("Creating" + threadName);
     }
 
+    @Override
     public void run(){
         System.out.println("Running" + threadName);
 
@@ -55,79 +45,64 @@ class ElevatorOperate implements Runnable {
     }
 
     public void pushFloorNumber(int number) {
-        elevator.floorNumberList[number] = "on";
+        elevator.destFloorList[number] = "on";
+    }
+
+    public boolean isReqFloor(ElevatorOperate elevatorOperate, int i){
+        if (elevatorOperate.getReqFloorNumber() == i){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEvDestFloor(String[] destFloorList, int i){
+        if (destFloorList[i] != null && destFloorList[i].equals("on")){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void goUpDownElevator(ElevatorOperate elevatorOperate) {
-        String[] floorNumberList = elevator.floorNumberList;
-        int srcFloorNumber = elevator.getSrcFloorNumber();
-        int destFloorNumber = elevator.getDestFloorNumber();
+        String[] evDestFloorList = elevator.destFloorList;
+        int evSrcFloor = elevator.getSrcFloor();
+        int evLastDestFloor = 1;
 
-        for (int i = 0; i < floorNumberList.length; i++) {
-            // 엘리베이터에서 버튼이 눌려있는 층수
-            if (floorNumberList[i] != null && floorNumberList[i].equals("on")) {
-                destFloorNumber = i;
+        for (int i=1; i<evDestFloorList.length; i++){
+            if (evDestFloorList[i]!=null && evDestFloorList[i].equals("on")) {
+                evLastDestFloor = i;
             }
         }
 
-        // 엘리베이터 층수보다 대상 층수가 큰 경우, up
-        if (srcFloorNumber < destFloorNumber) {
-            for (int j = srcFloorNumber; j <= destFloorNumber; j++){
-                int reqFloorNumber = elevatorOperate.getReqFloorNumber();
-                boolean isPushUpBtn = elevatorOperate.isPushUpBtn();
+        try {
+            for (int i=evSrcFloor; i<=evLastDestFloor; i++) {
+                System.out.println(i + "층");
+                Thread.sleep(200);
 
-                try {
-                    if (reqFloorNumber == j && isPushUpBtn){
-                        System.out.println(reqFloorNumber + "층에서 잠시 멈춥니다.");
-                        Thread.sleep(1000);
-                        continue;
-                    }
-
-                    Thread.sleep(250);
-                    System.out.println("엘리베이터: " + j + "층");
-                } catch (Exception e){
-                    e.printStackTrace();
+                if (isReqFloor(elevatorOperate, i)
+                        || isEvDestFloor(evDestFloorList, i)) {
+                    System.out.println(i + "층에 도착했습니다.");
+                    Thread.sleep(1000);
                 }
             }
-
-        // 엘리베이터 층수보다 대상 층수가 작은 경우, down
-        } else if (srcFloorNumber >= destFloorNumber) {
-
-
-        // 동일한 경우, stop
-        } else {
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-
-    public void pushFloorUpBtn(){
-
-    }
-
-    public void stopElevator(){
-
     }
 }
 
 class Elevator {
-    String[] floorNumberList = new String[11];  // TODO: 추후 변경
-    int srcFloorNumber = 1;
-    int destFloorNumber = 1;
+    int MaxFloor = 20;
+    String[] destFloorList = new String[MaxFloor+1];
+    int srcFloor = 1;
 
-    public int getSrcFloorNumber() {
-        return srcFloorNumber;
+    public int getSrcFloor() {
+        return srcFloor;
     }
 
-    public void setSrcFloorNumber(int srcFloorNumber) {
-        this.srcFloorNumber = srcFloorNumber;
-    }
-
-    public int getDestFloorNumber() {
-        return destFloorNumber;
-    }
-
-    public void setDestFloorNumber(int destFloorNumber) {
-        this.destFloorNumber = destFloorNumber;
+    public void setSrcFloor(int srcFloor) {
+        this.srcFloor = srcFloor;
     }
 }
 
@@ -135,9 +110,11 @@ public class TestElevatorOperate {
     /*
      * [v] 사용자가 엘리베이터에서 특정층을 누른다 (10층)
      * [v] 엘리베이터가 올라간다 (1층 > 10층)
-     * [v] 사용자가 오름버튼을 누른다 (7층)
-     * [v] 엘리베이터가 요청이 있는 층에서 멈춘다 (7층)
-     * [v] 엘리베이터가 올라간다 (10층)
+     * [v] 사용자가 오름버튼을 누른다 (4층)
+     * [v] 엘리베이터가 요청이 있는 층에서 멈춘다 (4층)
+     * [v] 사용자가 특정층을 누른다 (8층)
+     * [v] 엘리베이터가 특정층에서 멈춘다 (8층)
+     * [v] 엘리베이터가 특정층에서 멈춘다 (10층)
      */
     public static void main(String args[]) {
         ElevatorOperate elevatorOperate = new ElevatorOperate("1호기");
@@ -145,20 +122,10 @@ public class TestElevatorOperate {
         elevatorOperate.start();
 
         try {
-            Thread.sleep(1000);
-            elevatorOperate.setReqFloorNumber(7);
-            elevatorOperate.setPushUpBtn(true);
-            elevatorOperate.start();
-
+            elevatorOperate.setReqFloorNumber(4);
+            elevatorOperate.pushFloorNumber(8);
         } catch (Exception e){
 
         }
-    }
-
-    @Test
-    public void unitTest(){     // error
-        ElevatorOperate elevatorOperate = new ElevatorOperate("1호기");
-        elevatorOperate.pushFloorNumber(10);
-        elevatorOperate.start();
     }
 }
